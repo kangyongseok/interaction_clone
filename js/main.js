675256409c0267a95ca1fbf02604b960f64a61ec
강용석 <kangyongsuek@gmail.com>
2020-12-20 23:40:07 +0900

    add: 애니메이션 코드 추가

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
     let yOffset = 0;
     let prevScrollHeight = 0;
     let currentScene = 0;
+    let enterNewScene = false;
 
     const sceneInfo = [
         // 스크롤의 높이를 정하는 배열
@@ -10,8 +11,15 @@
             scrollHeight: 0,
             heightNum: 5,
             objs: {
-                container: document.querySelector('.scroll_section_0')
-            }
+                container: document.querySelector('.scroll_section_0'),
+                messageA: document.querySelector('.scroll_section_0 .main_message.a'),
+                messageB: document.querySelector('.scroll_section_0 .main_message.b'),
+                messageC: document.querySelector('.scroll_section_0 .main_message.c'),
+                messageD: document.querySelector('.scroll_section_0 .main_message.d'),
+            },
+            values: {
+                messageA_opacity: [0, 1],
+            },
         },
         {
             type: 'normal',
@@ -55,22 +63,59 @@
         }
         document.body.setAttribute('id', `show_scene_${currentScene}`);
     }
+
+    const calcValues = (values, currentYOffSet) => {
+        let rv;
+        let scrollRatio = currentYOffSet / sceneInfo[currentScene].scrollHeight; 
+        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        return rv;
+    }
+
+    const playAnimation = () => {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffSet = yOffset - prevScrollHeight;
+
+        switch(currentScene) {
+            case 0:
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffSet);
+                console.log(messageA_opacity_in)
+                objs.messageA.style.opacity = messageA_opacity_in
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+
     const scrollLoop = () => { 
         prevScrollHeight = 0;
+        enterNewScene = false;
         for(let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show_scene_${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
             if (currentScene === 0) return;
+            enterNewScene = true;
             currentScene--;
             document.body.setAttribute('id', `show_scene_${currentScene}`);
         }
+
+        if (enterNewScene) return;
+
+        playAnimation();
     }
 
     window.addEventListener('scroll', () => {
