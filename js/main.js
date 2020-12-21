b36632c83e1456c6da79dcf9d8b1ebe7aed7d55f
강용석 <kangyongsuek@gmail.com>
2020-12-21 21:00:40 +0900

    add: 애니메이션 코드 추가

--- js/main.js
+++ js/main.js
@@ -18,7 +18,12 @@
                 messageD: document.querySelector('.scroll_section_0 .main_message.d'),
             },
             values: {
-                messageA_opacity: [0, 1],
+                messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+                messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }], 
+                messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+                messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }], 
             },
         },
         {
@@ -49,7 +54,13 @@
 
     const setLayout = () => {
         for(let i = 0; i < sceneInfo.length; i++) {
-            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            if (sceneInfo[i].type === 'sticky') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+                sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+            }
+            if (sceneInfo[i].type === 'normal') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+            }
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
 
@@ -66,8 +77,22 @@
 
     const calcValues = (values, currentYOffSet) => {
         let rv;
-        let scrollRatio = currentYOffSet / sceneInfo[currentScene].scrollHeight; 
-        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        const scrolleHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffSet / scrolleHeight;
+        if (values.length === 3) {
+            const partScrolleStart = values[2].start * scrolleHeight;
+            const partScrolleEnd = values[2].end * scrolleHeight;
+            const partScrollHeight = partScrolleEnd - partScrolleStart;
+            if (currentYOffSet >= partScrolleStart && currentYOffSet <= partScrolleEnd) {
+                rv = (currentYOffSet - partScrolleStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+            } else if (currentYOffSet < partScrolleStart) {
+                rv = values[0];
+            } else if (currentYOffSet > partScrolleEnd) {
+                rv = values[1];
+            }
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
         return rv;
     }
 
@@ -75,12 +100,20 @@
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffSet = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffSet / scrollHeight;
 
         switch(currentScene) {
             case 0:
-                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffSet);
-                console.log(messageA_opacity_in)
-                objs.messageA.style.opacity = messageA_opacity_in
+                if (scrollRatio <= 0.22) {
+                    // in
+                    objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffSet);
+                    objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_in, currentYOffSet)}%)`;
+                } else {
+                    // out
+                    objs.messageA.style.opacity = calcValues(values.messageA_translateY_out, currentYOffSet);
+                    objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_out, currentYOffSet)}%)`;
+                }
                 break;
             case 1:
                 break;
